5b736002f2503748182ec497596de88b6fa4bdef
Ryan-Zayne <phoenixastro97@gmail.com>
2026-04-15 11:45:08 +0100

    chore: optional BullBoard dashboard

--- apps/backend/src/app/index.ts
+++ apps/backend/src/app/index.ts
@@ -1,4 +1,5 @@
 import { consola } from "consola";
+import { ENVIRONMENT } from "@/config/env";
 import { createHonoApp } from "@/lib/hono";
 import { appointmentsRoutes } from "./appointments/routes";
 import { authRoutes } from "./auth/routes";
@@ -28,12 +29,13 @@ app.basePath("/api/v1")
 	.route("", authRoutes)
 	.route("", appointmentsRoutes);
 
-try {
-	const { createBullBoardSetup } = await import("@/services/queues/utils/bullBoard");
-	const bullBoardSetup = createBullBoardSetup();
-	app.route(bullBoardSetup.baseQueuesPath, bullBoardSetup.queuesServerAdapter.registerPlugin());
-} catch (error) {
-	consola.error(new Error(`Failed to load bullboard`, { cause: error }));
+if (ENVIRONMENT.NODE_ENV === "development") {
+	try {
+		const { createBullBoardSetup } = await import("@/services/queues/utils/bullBoard");
+		const bullBoardSetup = createBullBoardSetup();
+		app.route(bullBoardSetup.baseQueuesPath, bullBoardSetup.queuesServerAdapter.registerPlugin());
+	} catch (error) {
+		consola.error(new Error(`Failed to load bullboard`, { cause: error }));
+	}
 }
-
 export { app };
