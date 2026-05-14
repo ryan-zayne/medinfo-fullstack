8669571a70dcd11055d27ae7c1d8330a103a93bb
Ryan-Zayne <phoenixastro97@gmail.com>
2026-05-14 13:23:13 +0100

    updates

--- apps/frontend/app/(home)/layout.tsx
+++ apps/frontend/app/(home)/layout.tsx
@@ -1,19 +1,12 @@
-"use client";
-
-import { sessionQuery } from "@/lib/react-query/queryOptions";
-import { HydrationBoundary } from "../HydrationBoundary.client";
 import { Footer, NavBar } from "./-components";
 
 function HomeLayout({ children }: LayoutProps<"/">) {
 	return (
-		// FIXME - Use the server component one if you can fix the issue coming from next-forward-cookies plugin, which makes pages unable to be statically generated
-		<HydrationBoundary onPrefetch={(client) => client.prefetchQuery(sessionQuery())}>
-			<div className="flex min-h-svh w-full flex-col items-center">
-				<NavBar />
-				{children}
-				<Footer />
-			</div>
-		</HydrationBoundary>
+		<div className="flex min-h-svh w-full flex-col items-center">
+			<NavBar />
+			{children}
+			<Footer />
+		</div>
 	);
 }
 
